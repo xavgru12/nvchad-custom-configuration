c2f014a7d9da56530fce70fbfe66e4b61e86e288
Xaver Gruber <xaver.gruber@curtis.ch>
2025-02-21 08:26:29 +0100

    fix issues and simplify

--- mappings.lua
+++ mappings.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M.general = {
-  n = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>",  "window left" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", "window down" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", "windown up" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", "window right" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  }
+ n = {
+        ["<C-h>"] = { "<cmd>TmuxNavigateLeft<cr>", "Window left" },
+        ["<C-j>"] = { "<cmd>TmuxNavigateDown<cr>", "Window down" },
+        ["<C-k>"] = { "<cmd>TmuxNavigateUp<cr>", "Window up" },
+        ["<C-l>"] = { "<cmd>TmuxNavigateRight<cr>", "Window right" },
+        ["<C-\\>"] = { "<cmd>TmuxNavigatePrevious<cr>", "Window previous" },
+    }
 }
 
 return M
